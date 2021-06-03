e50e5372ae3bf22fdd3f484bf65b57728793b1dc
Sowmiya S <sowmiyas605@gmail.com>
2021-06-03 10:47:51 +0530

    webpage changes

---
+++ webapp.py
@@ -0,0 +1,11 @@
+#Using Flask
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/500")
+def enter():
+  return "this is my new webpage"
+
+#pass this if this is the main function
+if __name__ == '__main__':
+  application.run(debug=True)
