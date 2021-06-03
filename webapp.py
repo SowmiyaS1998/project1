371082b0c08363023ad7f3e8812206a86dbcce73
Sowmiya S <sowmiyas605@gmail.com>
2021-06-03 11:03:06 +0530

    debug is set as true

--- webapp.py
+++ webapp.py
@@ -2,9 +2,9 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route("/500")
+@application.route("/5000")
 def enter():
-  return "this is my new webpage"
+  return "this is my new change in webpage"
 
 #pass this if this is the main function
 if __name__ == '__main__':
