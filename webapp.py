ed127e3d8e330425d635b18027e56cdf5d77b23d
Sowmiya S <sowmiyas605@gmail.com>
2021-06-04 10:46:47 +0530

    commit

--- webapp.py
+++ webapp.py
@@ -3,17 +3,10 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/500")
+
 def enter():
-<<<<<<< HEAD
-  return "this is my new change in web application page"
+    return "this is my new change in web application page"
 
 #pass this if this is the main function
 if __name__ == '__main__':
   application.run(debug=True)
-=======
-  return "this is my new webpage"
-
-#pass this if this is the main function
-if __name__ == '__main__':
-  application.run()
->>>>>>> 47a2b0d4c0c3fa3903e82e6f1bb5678e7357c037
